1be192eac174d08d33dd62a015870898dd687e56
AaronWu-train <aaronwu0218@gmail.com>
2025-01-13 14:27:34 +0800

    feat: Add dynamic routing and new views for NotFound, Select, Settings, and Create

---
+++ src/views/Settings.vue
@@ -0,0 +1,14 @@
+<!-- Settings.vue -->
+<!-- This is the temporary template file for adding a new page. -->
+
+<template>
+  <div>
+    <h1>Global Settings</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* 當前頁面的樣式 */
+</style>
