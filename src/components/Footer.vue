4fc2b2c437391d038c537947c9f5d579a3d4545a
MiaYu1102 <mia.c.e.yu@gmail.com>
2025-01-15 11:59:50 +0800

    feat: Enhance Footer and Navbar styles for improved layout and responsiveness

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,4 +12,15 @@ export default {
 
 <style scoped>
 /* 頁尾樣式 */
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #232f47;
+  color: white;
+  text-align: right;
+  box-sizing: border-box;
+  padding: 0px 30px;
+}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav class="navbar">
     <ul>
       <li><router-link to="/">Home</router-link></li>
       <li><router-link to="/about">About</router-link></li>
@@ -15,4 +15,33 @@ export default {
 
 <style scoped>
 /* 導覽列樣式 */
+.navbar {
+  display: flex;
+  justify-content: flex-end; /* 內容靠右對齊 */
+  padding: 10px 20px; /* 增加上下和左右內距 */
+  border-bottom: 2px solid #cfcfcf; /* 下方添加淺灰色線條 */
+}
+
+.navbar ul {
+  list-style: none; /* 移除列表項的圓點樣式 */
+  display: flex; /* 橫向排列 */
+  margin: 0; /* 移除外邊距 */
+  padding: 0; /* 移除內邊距 */
+}
+
+.navbar li {
+  margin-left: 30px; /* 每個項目之間留間距 */
+}
+
+.navbar a {
+  text-decoration: none; /* 移除連結的底線 */
+  color: #333; /* 預設文字顏色 */
+  font-size: 1em;
+  font-weight: 500;
+  transition: color 0.3s; /* 加入平滑變色效果 */
+}
+
+.navbar a:hover {
+  color: #007bff; /* 滑鼠懸停變為藍色 */
+}
 </style>
