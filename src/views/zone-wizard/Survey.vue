5adfbf3f11da18f6312ffeb357f66517b9526d0d
MiaYu1102 <mia.c.e.yu@gmail.com>
2025-01-19 16:18:28 +0800

    feat: 製作create頁面骨架

---
+++ src/views/zone-wizard/Survey.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRouter } from "vue-router";
+import { ElButton, ElUpload } from "element-plus";
+
+const router = useRouter();
+
+function navigateTo(page) {
+  router.push(`/${page}`);
+}
+</script>
+
+<template>
+  <div>
+    <h1>Survey</h1>
+    <!-- 跳轉介面的按鈕 -->
+    <el-button type="success" @click="navigateTo('check')">Next</el-button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.upload-section {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/zone-wizard/Upload.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { useRouter } from "vue-router";
+import { ElButton, ElUpload } from "element-plus";
+
+const router = useRouter();
+
+function navigateTo(page) {
+  router.push(`/${page}`);
+}
+
+const uploadFiles = (file) => {
+  console.log("Uploaded folder:", file);
+};
+</script>
+
+<template>
+  <div>
+    <h1>Upload</h1>
+
+    <!-- Upload 功能 -->
+    <el-upload
+      action="#"
+      :auto-upload="false"
+      directory
+      :on-change="uploadFiles"
+      class="upload-section"
+    >
+      <el-button type="primary">Click to Upload Folder</el-button>
+    </el-upload>
+
+    <!-- 跳轉介面的按鈕 -->
+    <el-button type="success" @click="navigateTo('survey')">Next</el-button>
+  </div>
+</template>
+
+<style scoped>
+.upload-section {
+  margin-bottom: 20px;
+}
+</style>
