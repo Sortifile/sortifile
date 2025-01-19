a56f0121ed661aac675cc1cf56388f265f489d0a
MiaYu1102 <mia.c.e.yu@gmail.com>
2025-01-19 16:18:28 +0800

    feat: 製作create頁面骨架

---
+++ src/views/zone-wizard/CheckRule.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRouter } from "vue-router";
+import { ElButton, ElUpload } from "element-plus";
+
+const router = useRouter();
+
+function navigateTo(page) {
+  router.push(`/${page}`);
+}
+</script>
+
+<template>
+  <div>
+    <h1>Check</h1>
+    <!-- 跳轉介面的按鈕 -->
+    <el-button type="success" @click="navigateTo('select')">Next</el-button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.upload-section {
+  margin-bottom: 20px;
+}
+</style>
