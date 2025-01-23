bafe3a7a21a64e0da39e11155e6b11823cf535e7
AaronWu-train <aaronwu0218@gmail.com>
2025-01-23 17:11:42 +0800

    feat: implement theme mode selection in Settings.vue and add dark theme support

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
+import "element-plus/theme-chalk/dark/css-vars.css";
+
 const app = createApp(App);
 app.use(router);
 app.use(ElementPlus);
