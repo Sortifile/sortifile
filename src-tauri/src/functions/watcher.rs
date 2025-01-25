85a09402b942dae9d2635a7ce18d170098ededb1
ckeisc43rd-cjtsai <jimtsai9596@gmail.com>
2025-01-25 18:35:09 +0800

    feat: basic watcher

---
+++ src-tauri/src/functions/watcher.rs
@@ -0,0 +1,27 @@
+use notify::{Event, RecursiveMode, Result, Watcher};
+use std::sync::mpsc;
+use std::path::Path;
+
+
+fn watch_directory() -> Result<()> {
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
+
+    // Use recommended_watcher() to automatically select the best implementation
+    // for your platform. The `EventHandler` passed to this constructor can be a
+    // closure, a `std::sync::mpsc::Sender`, a `crossbeam_channel::Sender`, or
+    // another type the trait is implemented for.
+    let mut watcher = notify::recommended_watcher(tx)?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+    // Block forever, printing out events as they come in
+    for res in rx {
+        match res {
+            Ok(event) => println!("event: {:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
+}
