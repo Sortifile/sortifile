3b8a9b484e252b926bf54b56a6f695fd82bc3a91
AaronWu-train <aaronwu0218@gmail.com>
2025-02-09 18:12:20 +0800

    feat: Finish problem 7 in survey

--- src/components/survey/archival_tendency.vue
+++ src/components/survey/archival_tendency.vue
@@ -4,10 +4,25 @@
 import { defineProps, defineEmits } from "vue";
 
 const props = defineProps({
-  modelValue: [String, Number, Array], // 綁定輸入值
+  modelValue: String, // 單選
 });
 
 const emit = defineEmits(["update:modelValue"]);
+
+const options = [
+  {
+    label: "A. 我重視經常使用的檔案，並傾向於放在容易訪問的位置。",
+    value: "A",
+  },
+  {
+    label: "B. 我重視長期存檔與保存，對文件的檔案結構更關心。",
+    value: "B",
+  },
+];
+
+const updateSelection = (value) => {
+  emit("update:modelValue", value);
+};
 </script>
 
 <template>
@@ -17,7 +32,22 @@ const emit = defineEmits(["update:modelValue"]);
         <h4 class="question-title">7. 下列何者較符合你的想法？</h4>
       </div>
     </template>
-    <div class="answer-area">test</div>
+    <div class="answer-area">
+      <el-radio-group
+        v-model="props.modelValue"
+        class="radio-group"
+        @update:modelValue="updateSelection"
+      >
+        <el-radio
+          v-for="(opt, index) in options"
+          :key="index"
+          :label="opt.value"
+          class="radio-item"
+        >
+          {{ opt.label }}
+        </el-radio>
+      </el-radio-group>
+    </div>
   </el-card>
 </template>
 
@@ -32,5 +62,16 @@ const emit = defineEmits(["update:modelValue"]);
 .question-card {
   margin-top: 16px;
   margin-bottom: 16px;
+}
+
+.answer-area {
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+
+.radio-group {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 </style>
