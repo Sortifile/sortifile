2095549458c91a7e524c0406d96e2a2d1ab95085
AaronWu-train <aaronwu0218@gmail.com>
2025-02-09 18:08:11 +0800

    feat: Finish problem 4 & 5 in survey

--- src/components/survey/folder_depth.vue
+++ src/components/survey/folder_depth.vue
@@ -1,12 +1,15 @@
-<!-- Problem 4 -->
 <script setup>
 import { defineProps, defineEmits } from "vue";
 
 const props = defineProps({
-  modelValue: [String, Number, Array], // 綁定輸入值
+  modelValue: Number, // 滑桿綁定數值
 });
 
 const emit = defineEmits(["update:modelValue"]);
+
+const updateSlider = (value) => {
+  emit("update:modelValue", value);
+};
 </script>
 
 <template>
@@ -16,7 +19,22 @@ const emit = defineEmits(["update:modelValue"]);
         <h4 class="question-title">4. 你希望檔案層次最多幾層？</h4>
       </div>
     </template>
-    <div class="answer-area">test</div>
+    <div class="answer-area">
+      <el-slider
+        :model-value="props.modelValue"
+        @update:modelValue="updateSlider"
+        :min="3"
+        :max="10"
+        show-stops
+        show-tooltip
+        :step="1"
+        class="slider"
+      />
+      <div class="slider-labels">
+        <span>很淺 (3層)</span>
+        <span>很深 (10層)</span>
+      </div>
+    </div>
   </el-card>
 </template>
 
@@ -31,5 +49,17 @@ const emit = defineEmits(["update:modelValue"]);
 .question-card {
   margin-top: 16px;
   margin-bottom: 16px;
+}
+
+.answer-area {
+  margin-top: 8px;
+  text-align: center;
+}
+
+.slider-labels {
+  display: flex;
+  justify-content: space-between;
+  font-size: 14px;
+  margin-top: 4px;
 }
 </style>
